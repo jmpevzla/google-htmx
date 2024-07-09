3223083eee870814af841ecea94580ca47b2c691
Jose Perez <jmperezvzla@gmail.com>
2024-07-09 17:43:26 -0400

    Htmx Google Search

---
+++ google.php
@@ -0,0 +1,24 @@
+<?php
+
+$url = "https://www.google.com";
+
+// Iniciar una nueva sesión cURL
+$ch = curl_init($url);
+
+// Configurar opciones de cURL
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Ejecutar la petición
+$response = curl_exec($ch);
+
+if ($response === false) {
+    // Hubo un error
+    echo "Error: " . curl_error($ch);
+} else {
+    $outputUtf8 = mb_convert_encoding($response, 'UTF-8', 'ISO-8859-1');
+    // Todo salió bien
+    echo $outputUtf8;
+}
+
+// Cerrar la sesión cURL
+curl_close($ch);
